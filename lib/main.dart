805cc658bef0aef5a7b7428afe8e3d788620240c
smitaag <smita.agarwal@outlook.com>
2020-10-09 15:15:01 +0530

    IOS App name changed and Crashlytics added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -20,8 +21,13 @@ import 'package:noq/utils.dart';
 //import 'services/authService.dart';
 
 //void main() => runApp(MyApp());
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  FlutterError.onError = await FirebaseCrashlytics.instance.recordFlutterError;
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
     runApp(new MyHome());
@@ -31,7 +37,7 @@ void main() {
 class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Firebase.initializeApp();
+    //Firebase.initializeApp();
     return MaterialApp(home: MyApp());
   }
 }
