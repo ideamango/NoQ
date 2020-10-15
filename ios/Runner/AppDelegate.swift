3ab771363b7f844c897f2164aa8d9606763c46f2
smitaag <smita.agarwal@outlook.com>
2020-10-15 23:06:14 +0530

    Dynamic links IOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,10 +8,41 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    FirebaseApp.configure()
+
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self 
     }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    func handleIncomingDynamicLink(_ dynamicLink: DynamicLink){
+        guard let url = dynamicLink.url else{
+            print("Thats wierd!!")
+            return
+        }
+        print("Your url param is \(url.absoluteString)")
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        if let incomingURL = userActivity.webpageURL{
+            print("Incoming URL is \(incomingURL)")
+            let linkHandled = DynamicLinks.dynamicLinks().handleUniversalLink(incomingURL ) {(dynamicLink, error) in guard error == nil else {
+                print("Found an error ! \(error!.localizedDescription)")
+                return
+                }
+                if let dynamicLink = dynamicLink{
+                    self.handleIncomingDynamicLink(dynamicLink)
+                }
+                            }
+            if linkHandled {
+                return true
+            }
+            else{
+                return false
+            }
+        }
+        return false
+    } 
 }
